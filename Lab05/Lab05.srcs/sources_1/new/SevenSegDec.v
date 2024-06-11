ac2dfaa3be55dacd978faa3942f9d969235908ac
Ahmed Farid <82676530+ahmedfarid59@users.noreply.github.com>
2024-06-11 18:05:46 +0300

    Started on lab 5

---
+++ Lab05/Lab05.srcs/sources_1/new/SevenSegDec.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/11/2024 04:48:05 PM
+// Design Name: 
+// Module Name: SevenSegDec
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SevenSegDec(
+    input [3:0]a, output [6:0] b
+    );
+    
+    assign b[6] = !((a[2] & ~a[1]&~a[0]) || (~a[3] & ~a[2] & ~a[1] & a[0]));
+    assign b[5] = !((a[2] & ~a[1] & a[0])|(a[2]&a[1]&~a[0]));
+    assign b[4] = !((a[3] & a[0]) | (~a[2] & a[1] & ~a[0]));
+    assign b[3] = 
+endmodule
